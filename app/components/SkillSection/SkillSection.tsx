1f92c2a5316477a517b4353074d12e361946baa7
NieHang <niehang77@gmail.com>
2026-06-01 21:28:14 +0800

    add linear-gradient animation to skill item

--- app/components/SkillSection/SkillSection.tsx
+++ app/components/SkillSection/SkillSection.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { motion } from "motion/react";
 
 export default function SkillSwiper() {
   const skills = [
@@ -13,15 +14,43 @@ export default function SkillSwiper() {
   return (
     <div className="flex items-center gap-2.5">
       {skills.map((skill) => (
-        <div
-          key={skill}
+        <motion.div
+          style={{
+            backgroundSize: "300% 100%",
+          }}
+          initial={{
+            backgroundImage: "none",
+            backgroundPosition: "0% 50%",
+          }}
+          whileHover={{
+            backgroundImage:
+              "linear-gradient(90deg, #3b82f6, #a855f7, #ec4899, #3b82f6)",
+            backgroundPosition: ["0% 50%", "100% 50%"],
+            transition: {
+              backgroundImage: { duration: 0 },
+              backgroundPosition: {
+                duration: 2,
+                ease: "linear",
+                repeat: Infinity,
+              },
+            },
+          }}
+          whileTap={{ scale: 0.95 }}
           className={clsx(
-            "cursor-pointer rounded-full bg-gray-100 px-3 py-1 text-sm",
-            "hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600",
+            "flex items-center justify-center",
+            "rounded-full p-0.5",
           )}
         >
-          {skill}
-        </div>
+          <div
+            key={skill}
+            className={clsx(
+              "cursor-pointer rounded-full bg-gray-100 px-3 py-1 text-sm",
+              "dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600",
+            )}
+          >
+            {skill}
+          </div>
+        </motion.div>
       ))}
     </div>
   );
